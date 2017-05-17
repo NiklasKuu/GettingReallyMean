8e9eeeed99139afb1b4d8ba63e6d0a8e615518d3
theonelegion <niklas.kuusisto95@gmail.com>
2017-05-17 18:35:14 +0300

    "finished readone review api moongoose controller"

---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,73 @@
+//required for MongoDB connection
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+//function that sends the parameter status and content as JSON
+var sendJSONresponse = function (res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+//Review API controller functions
+module.exports.reviewsCreate = function (req, res) {
+    sendJSONresponse(res, 200, {status: 'CreateReviewWorks'});
+};
+module.exports.reviewsReadOne = function (req, res) {
+    console.log("Getting single review");
+    if (req.params && req.params.locationid && req.params.reviewid) {
+        Loc
+                .findById(req.params.locationid)
+                //.select('name reviews')
+                .exec(
+                        function (err, location) {
+                            console.log(location);
+                            var response, review;
+                            if (!location) {
+                                sendJSONresponse(res, 404, {
+                                    "message": "locationid not found"
+                                });
+                                return;
+                            } else if (err) {
+                                sendJSONresponse(res, 400, err);
+                                return;
+                            }
+                            if (location.reviews && location.reviews.length > 0) {
+                                review = location.reviews.id(req.params.reviewid);
+                                
+
+
+                                if (!review) {
+                                    console.log(review);
+                                    sendJSONresponse(res, 404, {
+                                        "message": "reviewid not found"
+                                    });
+                                } else {
+                                    response = {
+                                        location: {
+                                            name: location.name,
+                                            id: req.params.locationid
+                                        },
+                                        review: review
+                                    };
+                                    console.log("this should work");
+                                    sendJSONresponse(res, 200, response);
+                                }
+                            } else {
+                                sendJSONresponse(res, 404, {
+                                    "message": "No reviews found"
+                                });
+                            }
+                        }
+                );
+    } else {
+        sendJSONresponse(res, 404, {
+            "message": "Not found, locationid and reviewid are both required"
+        });
+    }
+};
+module.exports.reviewsUpdateOne = function (req, res) {
+    sendJSONresponse(res, 200, {status: 'UpdateReviewWorks'});
+};
+module.exports.reviewsDeleteOne = function (req, res) {
+    sendJSONresponse(res, 200, {status: 'DeleteReviewWorks'});
+};
